ae4b22de60559c4a73aa041af9ee49738ebf2530
declanomeara <declanomeara@yahoo.co.uk>
2025-12-26 23:13:07 +0000

    DataService class implemented

---
+++ src/app/services/my-data.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage-angular';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MyDataService {
+
+  // This service will use ionic storage refer to ionic storage
+
+  constructor(private storage: Storage ){//boiler plate get/set
+    this.init();
+    }
+
+    async init(){
+      await this.storage.create(); //from storage
+    }
+
+    async get(key:string){
+      return await this.storage.get(key);
+
+    }
+
+    async set(key:string, value:any){
+      await this.storage.set(key, value);
+    }
+  
+}
