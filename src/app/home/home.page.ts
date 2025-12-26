41be71d258e93516b08bc4c4ee050d829e4358ee
declanomeara <declanomeara@yahoo.co.uk>
2025-12-26 12:50:57 +0000

    Add iconicons in homepage top bar and routing to relevant page once clicked

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonSearchbar, IonButton } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonSearchbar, IonButton, IonButtons, IonIcon } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';//allows to add only the icons I need
+import { heart, settings } from 'ionicons/icons';
+import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonSearchbar, IonButton],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonSearchbar, IonButton, IonButtons, IonIcon, RouterLink],
 })
 export class HomePage {
-  constructor() {}
+  constructor() {//get icons on creation of page
+     addIcons({ heart, settings });
+  }
 }
