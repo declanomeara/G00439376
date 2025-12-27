3f0e1a8332c90b9950a4a5a04b397f7efe3599b7
declanomeara <declanomeara@yahoo.co.uk>
2025-12-27 18:58:04 +0000

    homepage API call working bring back comma seperated values recipes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,18 +1,38 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonSearchbar, IonButton, IonButtons, IonIcon } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonSearchbar, IonButton, IonButtons, IonIcon, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';//allows to add only the icons I need
 import { heart, settings } from 'ionicons/icons';
 import { RouterLink } from '@angular/router';
+import { MyDataService } from '../services/my-data.service';
+import { MyHttpService } from '../services/my-http.service';
+import { HttpOptions } from '@capacitor/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonSearchbar, IonButton, IonButtons, IonIcon, RouterLink],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonSearchbar, IonButton, IonButtons, IonIcon, RouterLink, FormsModule, IonCard, IonCardHeader, IonCardTitle, CommonModule],
 })
 export class HomePage {
-  constructor() {//get icons on creation of page
-     addIcons({ heart, settings });
+  apiKey:string = "&apiKey=70759a4f7911402abcc53d3c51d3b759";
+  recipeInfo!: any;
+  ingredientRequest:string = "";
+
+  optionsSearch: HttpOptions ={ 
+    url: "https://api.spoonacular.com/recipes/complexSearch?query=" 
+  }
+ 
+  constructor(private ds: MyDataService, private mhs: MyHttpService,) {
+     addIcons({ heart, settings }/*get icons on creation of page*/) ;
+  }
+
+  async getReceipesByIngredients(){
+  
+    this.optionsSearch.url= this.optionsSearch.url.concat(this.ingredientRequest) + this.apiKey;
+    let result = await this.mhs.get(this.optionsSearch);
+    this.recipeInfo = result.data.results;
   }
 }
