d18522f58efa436c849228e9128e1c6a3e5aa174
declanomeara <declanomeara@yahoo.co.uk>
2025-12-30 19:47:38 +0000

    Add to Favourite Buttom logic implemented. Get/set favourite to sroage implemented

--- src/app/recipe-details/recipe-details.page.ts
+++ src/app/recipe-details/recipe-details.page.ts
@@ -1,49 +1,87 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonCardHeader, IonList, IonItem, IonThumbnail, IonLabel, IonCardTitle, IonCardSubtitle, IonCardContent, IonImg } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonCardHeader, IonList, IonItem, IonThumbnail, IonLabel, IonCardTitle, IonCardSubtitle, IonCardContent, IonImg, IonButton, IonIcon } from '@ionic/angular/standalone';
 import { MyDataService } from '../services/my-data.service';
 import { MyHttpService } from '../services/my-http.service';
 import { HttpOptions } from '@capacitor/core';
+import { addIcons } from 'ionicons';//allows to add only the icons I need
+import { heart, heartOutline } from 'ionicons/icons';
+
 
 @Component({
   selector: 'app-recipe-details',
   templateUrl: './recipe-details.page.html',
   styleUrls: ['./recipe-details.page.scss'],
   standalone: true,
-  imports: [IonContent, CommonModule, FormsModule, IonList, IonItem, IonLabel, IonCard, IonCardHeader, IonCardSubtitle, IonCardContent, IonImg, IonHeader, IonToolbar, IonTitle]
+  imports: [IonContent, CommonModule, FormsModule, IonList, IonItem, IonLabel, IonCard, IonCardHeader, IonCardSubtitle, IonCardContent, IonImg, IonHeader, IonToolbar, IonTitle, IonButton, IonIcon]
 })
 export class RecipeDetailsPage implements OnInit {
 
-  recipeId!:string;
-  apiKey:string = "?apiKey=70759a4f7911402abcc53d3c51d3b759";
+  recipeId!: string;
+  apiKey: string = "?apiKey=70759a4f7911402abcc53d3c51d3b759";
   recipeDetail!: any;
-  measureType:string ="";
+  measureType: string = "";
+  isFavourite: boolean = false;
+  favourites: any[] = [];
 
-  optionsRecipeDetails: HttpOptions={
+  optionsRecipeDetails: HttpOptions = {
     url: "https://api.spoonacular.com/recipes/"
   }
 
-  constructor(private ds: MyDataService,private mhs: MyHttpService) { }
+  constructor(private ds: MyDataService, private mhs: MyHttpService) { 
+    addIcons({ heart, heartOutline });
+  }
 
   ngOnInit() {
     this.getRecipeDetail();
 
   }
 
-  async getRecipeDetail(){
+  async getRecipeDetail() {
+
+    //get recipe id from storage
     this.recipeId = await this.ds.get('selectedRecipeId');
+
+    //get measurement preference from storage - default is metric
     this.measureType = await this.ds.get('measureType');
     this.measureType = this.measureType ?? 'metric';
 
+    //construct API url
     this.optionsRecipeDetails.url = this.optionsRecipeDetails.url.concat(this.recipeId) + "/information" + this.apiKey
-    
-    let result = await this.mhs.get(this.optionsRecipeDetails);
 
+    //API call to get recipe details
+    let result = await this.mhs.get(this.optionsRecipeDetails);
     this.recipeDetail = result.data;
-    console.log(this.recipeDetail);
-  
-   
+
+    //get favourites from storage returns items or empty array
+    this.favourites = await this.ds.get('favourites') ?? [];
+
+    //check to see it current recipe is already a favourite
+    for (let fav of this.favourites) {
+      if (fav.id === this.recipeDetail.id) {
+        this.isFavourite = true;
+        break;
+      }
+    }
   }
 
-}
+   async toggleFavourite(){
+
+    if (this.isFavourite) {
+    // If favourite already remove as favourite create a new array
+    this.favourites = this.favourites.filter(
+      fav => fav.id !== this.recipeDetail.id
+    );
+  } else {
+    //add as favourite if not favourire already
+    this.favourites.push(this.recipeDetail);
+  }
+    // persist as new favourite
+    await this.ds.set('favourites', this.favourites);
+
+    // toggle favourite state
+    this.isFavourite = !this.isFavourite;
+  
+   }
+  }
