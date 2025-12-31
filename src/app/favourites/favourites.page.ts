cd6dd89d057017a3081e9606c6088e7943223ab0
declanomeara <declanomeara@yahoo.co.uk>
2025-12-31 22:18:18 +0000

    Add Trashcan icon to Favourites toolbar. Clicking clears all favourite recipies

--- src/app/favourites/favourites.page.ts
+++ src/app/favourites/favourites.page.ts
@@ -1,22 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonCardHeader, IonButton, IonCardTitle } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonCardHeader, IonButton, IonCardTitle, IonButtons, IonIcon } from '@ionic/angular/standalone';
 import { MyDataService } from '../services/my-data.service';
 import { Router } from '@angular/router';
+import { addIcons } from 'ionicons';
+import { trash } from 'ionicons/icons';
 
 @Component({
   selector: 'app-favourites',
   templateUrl: './favourites.page.html',
   styleUrls: ['./favourites.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonCard, IonCardHeader, IonButton, IonCardTitle]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonCard, IonCardHeader, IonButton, IonCardTitle, IonButtons, IonIcon]
 })
 export class FavouritesPage  {
   
   favourites: any[] =[];
 
-  constructor(private ds: MyDataService, private router: Router) { }
+  constructor(private ds: MyDataService, private router: Router) { 
+    addIcons({trash});
+  }
 
   async ionViewWillEnter() {
     //get favourites from storage or return an empty array
@@ -26,6 +30,12 @@ export class FavouritesPage  {
   viewDetails(recipe:any){
     this.ds.set('selectedRecipeId', recipe.id);
     this.router.navigate(['/recipe-details']);
+  }
+
+  async clearAllFavourites(){//clear all saved favourites
+    await this.ds.set('favourites', []);
+    this.favourites =[];
+
   }
 
 }
